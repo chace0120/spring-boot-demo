3f56a5927c954d0966a93c3ed4cfe3e3493ddca5
balanar_cx@163.com <chace0120@126.com>
2016-03-30 15:43:29 +0800

    1 新增了SpringBoot集成MyBatis的代码示例。 2 将MyBatis-Spring的整合配置由xml转为了Java Config。 3 测试了Enum在Mybatis中的默认类型转换。

---
+++ spring-boot-mybatis/src/main/java/cn/chace/www/springboot/mybatis/Application.java
@@ -0,0 +1,15 @@
+package cn.chace.www.springboot.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenx
+ * @date 2016/3/30
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
