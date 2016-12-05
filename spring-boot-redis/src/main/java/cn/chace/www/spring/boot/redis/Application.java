157bf2f70a68c5362a7b562578fef7a9829ba6e0
balanar_cx@163.com <chace0120@126.com>
2016-12-05 17:14:14 +0800

    新增SpringBoot Redis示例目录。

---
+++ spring-boot-redis/src/main/java/cn/chace/www/spring/boot/redis/Application.java
@@ -0,0 +1,14 @@
+package cn.chace.www.spring.boot.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by chace0120 on 2016/3/22.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
